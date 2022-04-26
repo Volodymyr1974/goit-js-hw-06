c9258eb4a4d9ce267dfc91900aeb5acc3a35a74d
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-04-26 14:18:28 +0300

    додає рішення завдання №2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const elements = [];
+
+ingredients.forEach((el) => {
+  const navItemEl = document.createElement('li');
+  navItemEl.classList.add('item');
+  navItemEl.textContent = el;
+
+  elements.push(navItemEl);
+  
+});
+
+const ingredientsEl = document.querySelector('#ingredients');
+
+ingredientsEl.append(...elements);
+
+
+
+ 
