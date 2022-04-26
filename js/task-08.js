94870fbdf627134f41a7d200733800187c8fe9ca
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-04-27 00:44:50 +0300

    додає рішення завдання №8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+    
+    console.log(email.value); console.log(password.value);
+    if (email.value === "" || password.value === "") {
+        return alert("Error! fill in all the fields");
+    };
+
+    const FormData = {
+        name: email.value,
+        password: password.value,
+    };
+    console.log(FormData);
+    event.currentTarget.reset();
+   
+};
