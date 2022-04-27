a702a40fbaae83573dfeebd3c1fe331fc94795d7
Volodymyr Zvarych <Vbzvar@gmail.com>
2022-04-27 19:05:59 +0300

    додає рішення завдання №9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const btnChangeEl = document.querySelector('.change-color');
+const colorEl = document.querySelector('.color');
+const bodyColorEl = document.querySelector('body');
+
+
+btnChangeEl.addEventListener('click', ongetRandomHexColor);
+function ongetRandomHexColor() {
+  colorEl.textContent = `${getRandomHexColor()}`;
+  bodyColorEl.style.backgroundColor = `${getRandomHexColor()}`;
+};
